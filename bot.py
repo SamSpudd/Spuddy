96dc42a6f1ea6942a8b77d8a0450142c78f40605
SamSpudd <58823141+SamSpudd@users.noreply.github.com>
2020-07-07 19:27:05 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
+client = commands.Bot(command_prefix='-')
+
 client = discord.Client()
 
 @client.event
@@ -20,84 +22,74 @@ async def on_message(message):
         return
     if message.content == "Hello":
          await message.channel.send('Hi!')
-#if message.content == "-help":
-         embed = discord.Embed(title="Commands", description="Here are the commands that work with the bot!", color=0x00ff00)
-         embed.add_field(name="Generate Spotify", value="-gen spotify", inline=True)
-         embed.add_field(name="Generate Hulu", value="-gen hulu", inline=True)
-         embed.add_field(name="Generate Crunchyroll", value="-gen crunchyroll", inline=True)   
-         embed.add_field(name="Generate NordVPN", value="-gen nordvpn", inline=True)
-         embed.add_field(name="Generate Wakanim", value="-gen wakanim", inline=True)
-         embed.add_field(name="Generate Nitro", value="-gen nitro", inline=True)
-         embed.add_field(name="Generate DisneyPlus", value="-gen disney", inline=True)   
-         await message.channel.send(embed=embed)
-#if "Paid Spotify Premium" in [role.name for role in message.author.roles]:
-    #if message.content == "-gen spotify":
-        if os.path.exists('spotify.txt'):
-           lines = open('spotify.txt', encoding='utf-8').read().splitlines()
-           text = random.choice(lines)
-           await message.author.send(text)
-        else:
-            await message.channel.send("File Error. This shouldn't happen @SamSpudd#8226!")
-#           
-    #if message.content == "-gen hulu":
-        if os.path.exists('hulu.txt'):
-           lines = open('hulu.txt', encoding='utf-8').read().splitlines()
-           text = random.choice(lines)
-           await message.author.send(text)
-        else:
-            await message.channel.send("File Error. This shouldn't happen @SamSpudd#8226!")
-#            
-    #if message.content == "-gen crunchyroll":
-        if os.path.exists('crunchyroll.txt'):
-           lines = open('crunchyroll.txt', encoding='utf-8').read().splitlines()
-           text = random.choice(lines)
-           await message.author.send(text)
-        else:
-            await message.channel.send("File Error. This shouldn't happen @SamSpudd#8226!")
 #
-    #if message.content == "-gen nordvpn":
-        if os.path.exists('nordvpn.txt'):
-           lines = open('nordvpn.txt', encoding='utf-8').read().splitlines()
-           text = random.choice(lines)
-           await message.author.send(text)
-        else:
-           await message.channel.send("File Error. This shouldn't happen @SamSpudd#8226!")
-#      
-    #if message.content == "-gen wakanim":
-        if os.path.exists('wakanim.txt'):
-           lines = open('wakanim.txt', encoding='utf-8').read().splitlines()
-           text = random.choice(lines)
-           await message.author.send(text)
-        else:
-           await message.channel.send("File Error. This shouldn't happen @SamSpudd#8226!")
-#      
-    #if message.content == "-gen nitro":
-        if os.path.exists('nitro.txt'):
-           lines = open('nitro.txt', encoding='utf-8').read().splitlines()
-           text = random.choice(lines)
-           await message.author.send(text)
-        else:
-           await message.channel.send("File Error. This shouldn't happen @SamSpudd#8226!")
-#  
-    #if message.content == "-gen disney+":
-        if os.path.exists('disney.txt'):
-           lines = open('disney.txt', encoding='utf-8').read().splitlines()
-           text = random.choice(lines)
-           await message.author.send(text)
-        else:
-           await message.channel.send("File Error. This shouldn't happen @SamSpudd#8226!")
-# 
-const args = message.content.split(' ').slice(1); // All arguments behind the command name with the prefix
-const amount = args.join(' '); // Amount of messages which should be deleted
-
-if (!amount) return msg.reply('You haven\'t given an amount of messages which should be deleted!'); // Checks if the `amount` parameter is given
-if (isNaN(amount)) return msg.reply('The amount parameter isn`t a number!'); // Checks if the `amount` parameter is a number. If not, the command throws an error
-
-if (amount > 100) return msg.reply('You can`t delete more than 100 messages at once!'); // Checks if the `amount` integer is bigger than 100
-if (amount < 1) return msg.reply('You have to delete at least 1 message!'); // Checks if the `amount` integer is smaller than 1
-
-await msg.channel.messages.fetch({ limit: amount }).then(messages => { // Fetches the messages
-    msg.channel.bulkDelete(messages // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
-)});
+@client.event
+async def on_server_join(server):
+    print("Joining {0}".format(server.name))
 #
+@client.command(pass_context=True)
+async def help(ctx):
+    
+    embed = discord.Embed(
+        colour = discord.Colour.blue()
+    )
+
+    embed.add_field(name='-ping', value='Gives ping to client (expressed in ms)', inline=False)
+    embed.add_field(name='-kick', value='Kicks specified user', inline=False)
+    embed.add_field(name='-ban', value='Bans specified user', inline=False)
+    embed.add_field(name='-info', value='Gives information of a user', inline=False)
+    embed.add_field(name='-invite', value='Returns invite link of the client', inline=False)
+    embed.add_field(name='-clear', value='Clears an X amount of messages', inline=False)
+    await client.send_message(author, embed=embed)
+#
+@client.command(pass_context=True)
+async def clear(ctx, amount=10):
+    channel = ctx.message.channel
+    messages = []
+    async for message in client.logs_from(channel, limit=int(amount)):
+        messages.append(message)
+    await client.delete_messages(messages)
+    await client.say('Messages deleted')
+#
+@client.command(pass_context=True)
+async def ping(ctx):
+	channel = ctx.message.channel
+	t1 = time.perf_counter()
+	await client.send_typing(channel)
+	t2 = time.perf_counter()
+	embed=discord.Embed(title=None, description='Ping: {}'.format(round((t2-t1)*1000)), color=0x2874A6)
+	await client.say(embed=embed)
+
+@client.command(pass_context=True)
+async def info(ctx, user: discord.Member=None):
+    if user is None:
+        await client.say('Please input a user.')
+    else:
+        await client.say("The user's name is: {}".format(user.name) + "\nThe user's ID is: {}".format(user.id) + "\nThe user's current status is: {}".format(user.status) + "\nThe user's highest role is: {}".format(user.top_role) + "\nThe user joined at: {}".format(user.joined_at))
+
+@client.command(pass_context=True)
+async def kick(ctx, user: discord.Member=None):
+    author = ctx.message.author
+    if author.server_permissions.kick_members:
+        if user is None:
+            await client.say('Please input a user.')
+        else:
+            await client.say (":boot: Get kicked **{}**, Damn kid".format(user.name))
+            await client.kick(user)
+    else:
+        await client.say('You lack permission to preform this action')
+
+@client.command(pass_context=True)
+async def ban(ctx, user: discord.Member=None):
+    author = ctx.message.author
+    if author.server_permissions.kick_members:
+        if user is None:
+            await client.say('Please input a user.')
+        else:
+            await client.say("Get banned **{}**, Damn kid".format(user.name))
+            await client.ban(user)
+    else:
+        await client.say('You lack permission to preform this action')
+#
+
 client.run(token)
