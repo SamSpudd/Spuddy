42af84052e10933c71bb168a4926e99ad975d293
SamSpudd <58823141+SamSpudd@users.noreply.github.com>
2020-07-07 19:48:29 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,34 +19,15 @@ async def on_message(message):
         return
     if message.content == "Hello":
          await message.channel.send('Hi!')
-    if (message.content == CLEAR_MESSAGES) {
-
-      if (!message.channel.permissionsFor(message.author).hasPermission("MANAGE_MESSAGES")) {
-        message.channel.sendMessage("Sorry, you don't have the permission to execute the command \""+message.content+"\"");
-        console.log("Sorry, you don't have the permission to execute the command \""+message.content+"\"");
-        return;
-      } else if (!message.channel.permissionsFor(bot.user).hasPermission("MANAGE_MESSAGES")) {
-        message.channel.sendMessage("Sorry, I don't have the permission to execute the command \""+message.content+"\"");
-        console.log("Sorry, I don't have the permission to execute the command \""+message.content+"\"");
-        return;
-      }
+    if message.content == "-Clear":
         
-      if (message.channel.type == 'text') {
+      if message.channel.type == 'text':
         message.channel.fetchMessages()
-          .then(messages => {
-            message.channel.bulkDelete(messages);
-            messagesDeleted = messages.array().length; // number of messages deleted
+        message.channel.bulkDelete(messages);
+        messagesDeleted = messages.array().length;
 
-            // Logging the number of messages deleted on both the channel and console.
-            message.channel.sendMessage("Deletion of messages successful. Total messages deleted: "+messagesDeleted);
-            console.log('Deletion of messages successful. Total messages deleted: '+messagesDeleted)
-          })
-          .catch(err => {
-            console.log('Error while doing Bulk Delete');
-            console.log(err);
-          });
-      }
-    }
-  });
-});
+        message.channel.sendMessage("Deletion of messages successful. Total messages deleted: "+messagesDeleted);
+        console.log('Deletion of messages successful. Total messages deleted: '+messagesDeleted);
+          
 client.run(token)
+
